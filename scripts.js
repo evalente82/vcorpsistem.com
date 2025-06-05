a13110e2073f00f31cc11d404a53e481c144f120
Endrigo Valente <endrigo.valente@gmail.com>
2025-06-05 16:20:01 -0300

    Ajuste no site

--- scripts.js
+++ scripts.js
@@ -57,3 +57,39 @@ document.querySelectorAll('#sidebar a.nav-link').forEach(link => {
   });
 })();
 
+// Inicializa o segundo carrossel com Thumbs
+const swiperThumbs = new Swiper('.swiper-container-thumbs', {
+  slidesPerView: 3,   // Exibe 3 miniaturas
+  spaceBetween: 10,    // Espaço entre miniaturas
+  watchSlidesVisibility: true, // Detecta quais miniaturas estão visíveis
+  watchSlidesProgress: true,   // Atualiza o progresso das miniaturas
+  freeMode: true,      // Permite arrastar as miniaturas livremente
+  loop: true,          // Permite que a navegação seja cíclica
+  centeredSlides: true, // Centraliza as miniaturas
+  slideToClickedSlide: true, // Permite que ao clicar em uma miniatura, o slide principal seja alterado
+});
+
+// Inicializa o segundo carrossel (o carrossel principal) com Thumbs
+const swiperSecond = new Swiper('.swiper-container-second', {
+  loop: true,
+  slidesPerView: 1, // Exibe uma imagem por vez
+  spaceBetween: 10, // Espaço entre as imagens
+  autoplay: {
+    delay: 5000, // Intervalo entre cada slide
+    disableOnInteraction: false, // O autoplay não será desativado quando o usuário interagir
+  },
+  speed: 500, // Velocidade da transição
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // Tornando os pontos de navegação clicáveis
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  // Usando o Thumbs para controlar a navegação do carrossel principal
+  thumbs: {
+    swiper: swiperThumbs, // Referência ao carrossel de miniaturas
+  }
+});
